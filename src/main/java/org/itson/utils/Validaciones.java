a3782bbb9f83b1ec5fcb8558c0470fc09bd1321e
OscarMinjarez <oscarminjarez@outlook.com>
2023-04-12 15:05:35 -0700

    Se añadieron más validaciones para formatear objetos.

--- src/main/java/org/itson/utils/Validaciones.java
+++ src/main/java/org/itson/utils/Validaciones.java
@@ -4,4 +4,32 @@ public class Validaciones {
     public static boolean campoVacio(String campo) {
         return campo.isEmpty() || campo.isBlank();
     }
+    
+    public static boolean comprobarFormatoNombre(String nombre) {
+        for (int i = 0; i < nombre.length(); i++) {
+            char c = nombre.charAt(i);
+            
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public static boolean comprobarFormatoApellido(String apellido) {
+        for (int i = 0; i < apellido.length(); i++) {
+            char c = apellido.charAt(i);
+            
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public static boolean comprobarFormatoAnho(String anho) {
+        return anho.matches("\\d{4}");
+    }
 }
