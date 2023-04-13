51e39b88be1ab57a6d77ef6d10140c8b795b98c3
OscarMinjarez <oscarminjarez@outlook.com>
2023-04-13 16:15:48 -0700

    Se agregó una clase para obtener datos de costos.

---
+++ src/main/java/org/itson/utils/CostosTramites.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.utils;
+
+/**
+ *
+ * @author Oscar
+ */
+public class CostosTramites {
+    public static final Float COSTO_LICENCIA_UN_ANHO_NORMAL = 600f;
+    public static final Float COSTO_LICENCIA_DOS_ANHOS_NORMAL = 900f;
+    public static final Float COSTO_LICENCIA_TRES_ANHOS_NORMAL = 1100f;
+    
+    public static final Float COSTO_LICENCIA_UN_ANHO_DISCAPACITADO = 200f;
+    public static final Float COSTO_LICENCIA_DOS_ANHOS_DISCAPACITADO = 500f;
+    public static final Float COSTO_LICENCIA_TRES_ANHOS_DISCAPACITO = 700f;
+    
+    public static final Float COSTO_PLACA_AUTO_NUEVO = 1500f;
+    public static final Float COSTO_PLACA_AUTO_USADO = 1000f;
+}
