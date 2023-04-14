c908d345b32dd8ac86a7c6132554d874dca8883b
OscarMinjarez <oscarminjarez@outlook.com>
2023-04-13 23:06:55 -0700

    Se creó una clase específica para manejar fechas.

---
+++ src/main/java/org/itson/utils/ManejadorFechas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Oscar
+ */
+public class ManejadorFechas {
+    public static GregorianCalendar obtenerFechaActual() {
+        LocalDateTime fechaHora = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = fechaHora.atZone(ZoneId.systemDefault());
+        
+        return GregorianCalendar.from(zonedDateTime);
+    }
+    
+    public static GregorianCalendar sumarAnios(GregorianCalendar fecha, int anios) {
+        fecha.add(Calendar.YEAR, (int) anios);
+        
+        return fecha;
+    }
+}
