0b5b77b9f40610129d6e2a17087ca0c1e01f0900
OscarMinjarez <oscarminjarez@outlook.com>
2023-04-16 21:08:24 -0700

    Se creó un método para comprobar periodos.

--- src/main/java/org/itson/utils/ManejadorFechas.java
+++ src/main/java/org/itson/utils/ManejadorFechas.java
@@ -4,6 +4,7 @@
  */
 package org.itson.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -31,5 +32,30 @@ public class ManejadorFechas {
     public static boolean fechaActualEntre(Calendar fechaInicio, Calendar fechaFin) {
         Calendar fechaActual = Calendar.getInstance();
         return fechaActual.compareTo(fechaInicio) >= 0 && fechaActual.compareTo(fechaFin) <= 0;
+    }
+    
+    public static boolean estaEnPeriodo(Calendar fecha, Calendar fechaInicio, Calendar fechaFin) {
+        return fecha.after(fechaInicio) && fecha.before(fechaFin);
+    }
+    
+    public static boolean validarRangoFechas(Calendar desde, Calendar hasta) {
+        if (desde == null || hasta == null) {
+            return false;
+        }
+        
+        return desde.compareTo(hasta) < 0;
+    }
+    
+    public static Calendar convertirLocalDateACalendar(LocalDate fecha) {        
+        if (fecha == null) {
+            return null;
+        }
+        
+        LocalDate localDate = fecha;
+        LocalDateTime localDateTime = localDate.atStartOfDay();
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        GregorianCalendar gregorianCalendar = GregorianCalendar.from(zonedDateTime);
+        
+        return gregorianCalendar;
     }
 }
